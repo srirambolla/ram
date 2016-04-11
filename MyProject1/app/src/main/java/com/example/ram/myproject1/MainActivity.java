4411a19e151d387ac241a386ae868e09efd5927b
ram <srirambolla409@gmail.com>
2016-04-11 15:31:59 +0530

    tool bar added

--- MyProject1/app/src/main/java/com/example/ram/myproject1/MainActivity.java
+++ MyProject1/app/src/main/java/com/example/ram/myproject1/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.ram.myproject1;
 
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,26 +13,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+//
+//        DrawerLayout drawerLayout;
+//       ActionBarDrawerToggle toggle;
+////
+//        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        getSupportActionBar().setHomeButtonEnabled(true);
+//        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close) {
+//        };
+//
+//        toggle.syncState();
+//
+   }
 
-        DrawerLayout drawerLayout;
-        ActionBarDrawerToggle toggle;
-
-            drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setHomeButtonEnabled(true);
-            toggle = new ActionBarDrawerToggle(this, drawerLayout,R.string.open, R.string.close)
-            {
-            };
-
-            toggle.syncState();
-
+    public boolean onOptionsItemSelected(MenuItem item) {
+        ActionBarDrawerToggle toggle = null;
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
         }
-        public boolean onOptionsItemSelected(MenuItem item) {
-            ActionBarDrawerToggle toggle = null;
-            if (toggle.onOptionsItemSelected(item)) {
-                return true;
-            }
-             return super.onOptionsItemSelected(item);
-        }
+        return super.onOptionsItemSelected(item);
     }
+}
 
